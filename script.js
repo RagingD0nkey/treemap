e4340bd925ffe5bdf723c6062de5fd57d516cab4
RagingD0nkey <152647125+RagingD0nkey@users.noreply.github.com>
2024-09-15 00:05:05 +0200

    cleaning js file

--- script.js
+++ script.js
@@ -40,8 +40,7 @@ const cac40Components = [
     { symbol: "GFC", marketCap: 7.1 },    // Gecina
     { symbol: "IL", marketCap: 6.6 }      // Iliad
   ];
- 
- 
+
 function GetSquarifyTreeMap(data, width, height, keyData = "value"){
  
     function CheckArguments(data, width, height, keyData){
@@ -191,11 +190,79 @@ function GetSquarifyTreeMap(data, width, height, keyData = "value"){
    
     return finalData;
 }
- 
+
+function GetPivotTreemap(data,width,height,keyData = "value",pivot ="size"){
+
+    function CheckArguments(data, width, height, keyData){
+        if (width<0 || typeof(width)!== "number"){
+            throw new Error('Invalid or missing treemap width');
+        }
+        if (height<0 || typeof(height)!== "number"){
+            throw new Error('Invalid or missing treemap height');
+        }
+        if (!Array.isArray(data)){
+            throw new Error('Please pass treemap data as an Array');
+        }
+        if (data.length === 0){
+            throw new Error('Treemap data cannot be an empty Array');
+        }
+   
+        let cleanData = [];
+        data.forEach(item => {
+            if (typeof(item)==="number"){
+                cleanData.push({
+                    [keyData]: item,
+                    ["treemapPivot"]:{
+                        area:0,
+                        width:0,
+                        height:0,
+                        x:0,
+                        y:0
+                    }
+                })
+            }else if (typeof(item)==="object"){
+                if (item === null){
+                    throw new Error("Treemap data cannot be null");
+                }
+                if (!item.hasOwnProperty(keyData) || typeof(item[keyData])!=="number"){
+                    throw new Error("Treemap data has an incorrect or missing property name.");
+                }
+                item["treemapPivot"]={
+                    area:0,
+                    width:0,
+                    height:0,
+                    x:0,
+                    y:0
+                }
+                cleanData.push(item)
+            }
+        });
+   
+        return cleanData;
+    }
+
+    function ScaleData(data, width, height,keyData){
+        const totalArea = height * width;
+        const dataArea = data.reduce((sum,item)=> sum + item[keyData],0);
+   
+        data.forEach(item => {
+            item["treemapPivot"]["area"]= item[keyData]*totalArea/dataArea;
+        });
+   
+        return data;
+    }
+
+    let validatedData=CheckArguments(data, width, height, keyData) // Check validity of arguments
+    let scaledData = ScaleData(validatedData, width, height, keyData) // Scale data based on original rectangle dimensions
+
+    
+
+}
+
 let testwidth = 1800;
 let testheight = 800;
-result = GetSquarifyTreeMap(sp500Components,testwidth,testheight, "marketCap")
- 
+let result = GetSquarifyTreeMap(cac40Components,testwidth,testheight, "marketCap")
+
 // *****WIDTH + HEIGHT***** //
  
 // const frame=document.querySelector("div.frame")
@@ -267,4 +334,3 @@ result.forEach(item => {
  
     frame.appendChild(svg)
 });
- 
